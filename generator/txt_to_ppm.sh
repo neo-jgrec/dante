f45d13f7eadf4ede594dc53621d788e24e951e46
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-05 16:42:31 +0100

    feat(generator) add of a script that convert a maze into a .ppm image

---
+++ generator/txt_to_ppm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+input_file=$1
+output_file=$2
+
+# Get the width and height of the input file
+width=$(wc -w < $input_file)
+height=$(wc -l < $input_file)
+height=$(($height + 1))
+
+# Create the PPM header
+echo "P3" > $output_file
+echo "$width $height" >> $output_file
+echo "255" >> $output_file
+
+# Convert each character to a color and write to the output file
+while read -n 1 char; do
+    if [ "$char" = "o" ]; then
+        # Yellow
+        echo "255 255 0" >> $output_file
+    elif [ "$char" = "*" ]; then
+        # White
+        echo "255 255 255" >> $output_file
+    elif [ "$char" = "X" ]; then
+        # Black
+        echo "0 0 0" >> $output_file
+    fi
+done < $input_file
