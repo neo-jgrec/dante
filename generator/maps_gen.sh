94e614df4aeac4d1357bede5832df5230996ecaa
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-05 15:37:05 +0100

    feat(generator) script can be executed from root nor generator file nor solver file

--- generator/maps_gen.sh
+++ generator/maps_gen.sh
@@ -2,11 +2,15 @@
 # Generator tester Script by @jgreclevrai
 
 # Test folder
-folder=../mazes
-mkdir -p $folder
+folder=$(pwd | grep -q "generator" && echo "../maps" || echo "maps")
+mkdir -p $folder && rm -f $folder/*
 
-# # Makefile actions in silent mode if generator binary doesn't exist
-if [ ! -f generator ] ; then
+# Binary name can be generator or generator/generator or ../generator/generator
+bin_name=$(pwd | grep -q "generator" && echo "./generator" || echo "generator/generator")
+echo "Binary name: $bin_name"
+
+# Makefile actions in silent mode if generator binary doesn't exist
+if [ ! -f $bin_name ] ; then
     make re &> /dev/null
     make clean &> /dev/null
 fi
@@ -25,7 +29,7 @@ array_test=(\
 for test in "${array_test[@]}" ; do
     time_start=$(date +%s%N)
     file_name=$(echo $test | sed 's/ /_/g')
-    ./generator $test &> $folder/$file_name\
+    $bin_name $test &> $folder/$file_name\
     && (echo -en "\e[32m[OK]\e[0m \`$test\`" && time_end=$(date +%s%N) && time_diff_sec=$(echo "scale=3; ($time_end - $time_start) / 1000000000" | bc) && echo -e " (\e[33m$time_diff_sec\e[0ms)")\
     || (echo -e "\e[31m[KO]\e[0m \`$test\` " && rm -f $folder/$file_name)
 done
